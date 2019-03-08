d428f59a30dff3eb4592cf1add46389963edc668
Yucheng Lin <nolinyc@gmail.com>
2019-03-07 17:25:50 -0800

    Initial Commit

---
+++ cmpe131book/DataViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DataViewController.swift
+//  cmpe131book
+//
+//  Created by sean on 3/7/19.
+//  Copyright © 2019 SJSU CMPE131. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
