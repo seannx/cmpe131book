f70a143a89fae45a5d74d3601e5e27818fb854dc
Bo An <bo.an.563641292@gmail.com>
2019-04-03 18:33:29 -0700

    // add layout of bookinformation

--- cmpe131book/Controller/DetailTableViewController.swift
+++ cmpe131book/Controller/DetailTableViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 
 class DetailTableViewController: UITableViewController {
 
+    
+    @IBOutlet weak var T: UILabel!
+    @IBOutlet weak var author: UILabel!
+    @IBOutlet weak var Genre: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    @IBOutlet weak var pages: UILabel!
+    @IBOutlet weak var level: UILabel!
+    @IBOutlet weak var publisher: UILabel!
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var IBSN: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.rowHeight = 60
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -31,7 +37,6 @@ class DetailTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return 9
     }
-    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
---
+++ cmpe131book/cell/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  cmpe131book
+//
+//  Created by 安博 on 4/3/19.
+//  Copyright © 2019 SJSU CMPE131. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
